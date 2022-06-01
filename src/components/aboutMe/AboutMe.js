2643a59080922db3351eaf8664175465cac9d7bf
August Johnson <gusjohnson98@gmail.com>
2022-06-01 05:12:42 -0500

    started header and about me

---
+++ src/components/aboutMe/AboutMe.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutMe() {
+    return(
+        <div className="about-container">
+            <h1 className="about-header"></h1>
+            <p></p>
+        </div>
+    );
+}
+
+export default AboutMe;
